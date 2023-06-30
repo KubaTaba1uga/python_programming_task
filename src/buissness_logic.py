ff2849e65ecce22ffd830a88bacfe91f91c3546c
KubaTaba1uga <jakub@buczynski.net.pl>
2023-06-30 11:40:43 +0200

    code refactor

--- src/buissness_logic.py
+++ src/buissness_logic.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # from jwcrypto import jwk
 import python_jwt as jwt
+from jwcrypto.jwk import JWK as PrivateKey
 
 from src._constants import (
     UPSTREAM_IP,
@@ -25,13 +26,22 @@ def create_upstream_request(request):
 
 def handle_upstream_request(request):
     jwt_claims, new_request_payload = {
+        # satisfies task requirement about `jti`
         "jti": generate_unique_value(),
-        "iat": get_request_datetime(),
-    }, {"user": "username", "date": "todays date"}
+        # satisfies task requirement about `ati`
+        "iat": generate_datetime_value(),
+    }, {
+        # satisfies task requirement about `payload`.
+        #  Propably `user` should be generated but
+        #  that would require sharing db with upstream
+        #  I want to keep things simple for POC.
+        #  TO-DO
+        #  - get logged in `user` from request
+        "user": "username",
+        "date": "todays date",
+    }
 
     jwt = generate_jwt(jwt_claims)
-
-    print(jwt)
 
     return request
 
@@ -40,15 +50,19 @@ def generate_unique_value() -> str:
     return str(uuid.uuid4())
 
 
-def get_request_datetime() -> datetime:
+def generate_datetime_value() -> datetime:
     return datetime.now()
-
-
-def get_key() -> str:
-    return HEX_STRING_SECRET
 
 
 def generate_jwt(claims):
     return jwt.generate_jwt(
-        claims=claims, priv_key=HEX_STRING_SECRET, algorithm=SIGNATURE_ALGHORITM
+        claims=claims,
+        # satisfies task requirement about `hex as a secret`
+        priv_key=generate_private_key(),
+        # satisfies task requirement about `HS512`
+        algorithm=SIGNATURE_ALGHORITM,
     )
+
+
+def generate_private_key() -> PrivateKey:
+    return PrivateKey.from_password(HEX_STRING_SECRET)
